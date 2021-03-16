14668b08f6d20d4a009d6ad02ff00bae7a6660ae
itskhurram <itsmeks@gmail.com>
2021-03-16 13:12:59 -0400

    simple app working

--- userRepository.js
+++ userRepository.js
@@ -3,14 +3,26 @@
 const userSchema = require("./userSchema");
 
 module.exports = {
-  getUsers() {
-      return userSchema.find();
+  getUsers: () => {
+    return userSchema.find();
   },
-  saveUser(user) {
-      const schemaUser =  new userSchema(user);
-      schemaUser.save(schemaUser);
-  },
-  getUserbyEmail() {
+  getByEmail: (email) => {
     return userSchema.findOne({ email: email });
   },
+
+  async getUsersAsync() {
+    return await userSchema.find();
+  },
+  async getByEmailAsync(email) {
+    return userSchema.findOne({ email: email });
+  },
+
+  //   ,
+  //   saveUser(user) {
+  //       const schemaUser =  new userSchema(user);
+  //       schemaUser.save(schemaUser);
+  //   },
+  //   getUserbyEmail() {
+  //     return userSchema.findOne({ email: email });
+  //   },
 };
